542200ea1af362fb4eac0939624eeaaa3365ec4c
Aya Yan <yy040316@163.com>
2025-03-21 22:55:49 +0800

    更新布局，添加toolbar，添加并编辑了两个主题的背景，更改主题颜色，增强可读性

--- app/src/main/java/com/aya/entropycalc/MainActivity.java
+++ app/src/main/java/com/aya/entropycalc/MainActivity.java
@@ -1,17 +1,15 @@
 package com.aya.entropycalc;
 
 import android.os.Bundle;
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // 加载布局文件
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
